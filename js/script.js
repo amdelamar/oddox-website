2e7627aad10e0bac30c8612e0646bb3a0db7e075
amdelamar <amdelamar@protonmail.com>
2017-11-13 20:13:01 -0500

    Add initial page design

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// When the user clicks on the button, scroll to the top of the document
+function gotoTop() {
+  document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = 0; // For IE and Firefox
+} 
